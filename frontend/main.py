e66972a849baa68a9a8d4e0ae5eedb61c672725b
Logan Gorence <logan@gorence.io>
2021-03-25 18:45:00 -0700

    Flip flag to disable strict slashes in Flask.

--- frontend/main.py
+++ frontend/main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from routes.api.admin_challenges import bp as admin_challenges_bp
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(admin_challenges_bp, url_prefix="/api/admin/challenges")
 
 app.config[
