71e8cca51f8562840117a41c5e8eaed8f647a8a7
Logan Gorence <logan@gorence.io>
2021-03-25 18:45:00 -0700

    Flip flag to disable strict slashes in Flask.

--- frontend/routes/api/admin_challenges.py
+++ frontend/routes/api/admin_challenges.py
@@ -3,11 +3,11 @@ from flask import Blueprint, request
 bp = Blueprint("admin_challenges", __name__)
 
 
-@bp.route("", methods=["GET"])
+@bp.route("/", methods=["GET"])
 def api_admin_challenges_list():
     return "[]"
 
 
-@bp.route("", methods=["POST"])
+@bp.route("/", methods=["POST"])
 def api_admin_challenges_create():
     return {"test": 123}
