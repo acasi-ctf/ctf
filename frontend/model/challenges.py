2bfaef84b9c7077b49e267666ec55d6ae5f902d0
Logan Gorence <logan@gorence.io>
2021-04-03 16:39:16 -0700

    Work on challenge set / challenge REST API.

--- frontend/model/challenges.py
+++ frontend/model/challenges.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Text, text
-from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy import Column, ForeignKey, Text, text, PrimaryKeyConstraint, Integer
+from sqlalchemy.dialects.postgresql import BYTEA, JSONB, UUID
+from sqlalchemy.orm import relationship
 
 from frontend.extensions import db
 
@@ -10,10 +11,12 @@ class ChallengeSet(db.Model):
     id = Column(
         UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
     )
-    slug = Column(Text(), index=True, unique=True, nullable=False)
-    name = Column(Text(), nullable=False)
-    description = Column(Text(), nullable=False)
-    version = Column(Text(), nullable=False)
+    slug = Column(Text, index=True, unique=True, nullable=False)
+    name = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    version = Column(Text, nullable=False)
+
+    challenges = relationship("Challenge")
 
 
 class Challenge(db.Model):
@@ -22,13 +25,27 @@ class Challenge(db.Model):
     id = Column(
         UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")
     )
-    slug = Column(Text(), nullable=False)
+    slug = Column(Text, nullable=False)
     parent_id = Column(
         UUID(as_uuid=True),
         ForeignKey("challenge_set.id", ondelete="CASCADE"),
         nullable=False,
     )
     name = Column(Text(), nullable=False)
-    description = Column(Text(), nullable=False)
-    provisioner = Column(JSONB(), nullable=False)
-    documentation = Column(JSONB(), nullable=False)
+    description = Column(Text, nullable=False)
+    provisioner = Column(JSONB, nullable=False)
+
+    documentation = relationship("Documentation")
+
+
+class Documentation(db.Model):
+    __tablename__ = "documentation"
+    __table_args__ = (PrimaryKeyConstraint("parent_id", "path"),)
+
+    parent_id = Column(
+        UUID(as_uuid=True), ForeignKey("challenge.id", ondelete="CASCADE")
+    )
+    path = Column(Text, nullable=False)
+    order = Column(Integer, nullable=False)
+    name = Column(Text, nullable=False)
+    content = Column(BYTEA, nullable=False)
