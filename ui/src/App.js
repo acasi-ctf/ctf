b4c02991880a6693d45f7c55cbdc7a7c09bcea7c
Logan Gorence <logan@gorence.io>
2021-04-04 21:25:18 -0700

    Integrate with Auth0.

--- ui/src/App.js
+++ ui/src/App.js
@@ -9,29 +9,30 @@ import ChallengeSet1 from './Page/ChallengeSet1';
 import Terminal from './Terminal';
 
 
-
 export default function App() {
-  return (
-	  <>
-	  	<Router>
-            <div className='App'>
-                <PermanentDrawerLeft/>
-                <div className='contentBackground'>
-                    <div className='Content'>
-                        <Switch>
-                            <Route path='/' exact component={Home}/>
-                            <Route path='/LeaderBoard' component={leaderboard}/>
-                            <Route path='/ChallengeSet1' component={ChallengeSet1}/>
-                        </Switch>
+    return (
+        <>
+            <Router>
+                <div className='App'>
+                    <PermanentDrawerLeft/>
+                    <div className='contentBackground'>
+                        <div className='Content'>
+                            <Switch>
+                                <Route path='/' exact component={Home}/>
+                                <Route path='/LeaderBoard'
+                                       component={leaderboard}/>
+                                <Route path='/ChallengeSet1'
+                                       component={ChallengeSet1}/>
+                            </Switch>
+                        </div>
                     </div>
+
+                    {/* UNCOMMENT THIS TO TURN ON THE TERMINAL */}
+                    <Terminal/>
+
                 </div>
-
-                {/* UNCOMMENT THIS TO TURN ON THE TERMINAL */}
-                <Terminal/>
-
-            </div>
-		</Router>
-	  </>
-  );
+            </Router>
+        </>
+    );
 }
 
