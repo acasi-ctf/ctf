c2fa8387c2b3f5415d5d28f10032103442162df8
colbycocking <33988363+colbycocking@users.noreply.github.com>
2021-04-06 22:13:44 -0700

    added cipher python files to repo

---
+++ challenges/Challenge-Set-1/Challenges/Cipher-challenge/Letter-to-Number.py
@@ -0,0 +1,51 @@
+import random
+
+#random string
+def random_alphabet():
+
+    string = ""
+    for i in range(10):
+        string = string + random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    return string
+
+#convert letter to number
+def Convert_number(string):
+    
+    lists = list(string[:])
+    new_list = []
+ 
+    for i in lists: 
+        if i.isalpha():
+            
+            if 97 <= ord(i) < (122-4):
+                new_list.append(ord(i) - 96)
+            else:
+                new_list.append(ord(i) - 64)
+        else:
+            new_list.append(i)
+            
+    after = "".join((str(v)+" ") for v in new_list)
+    
+    return after
+
+def main():
+    string = random_alphabet()
+    code = Convert_number(string)
+    print("This is Letter to number Challenge!")
+    print("The code is:")
+    print(code)
+    print()
+
+    while(True):
+        strs = input("The decode text is: ")
+        if strs == string:
+            break
+        else:
+            print("Nope!Try Again!")
+            continue
+
+    print("Congrarulations! You get 10 point!")
+    
+
+if __name__ == "__main__":
+    main()
