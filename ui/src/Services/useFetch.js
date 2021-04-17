5dafbcec9dce056d8ad98aec649257b3e77ba3a7
colbycocking <33988363+colbycocking@users.noreply.github.com>
2021-04-16 22:33:27 -0700

    Added error protection to application. Added challenge card class, need to finish implementing state.

---
+++ ui/src/Services/useFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function init() {
+            try {
+                const response = await fetch(url);
+                if (response.ok) {
+                    const json = await response.json();
+                    setData(json);
+                } else {
+                    throw response;
+                }
+            } catch (e) {
+                setError(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        init();
+    }, [url]);
+
+    return { data, error, loading };
+}
