eee85add33eca68e49b26942d78ac7c6d46b9604
colbycocking <33988363+colbycocking@users.noreply.github.com>
2021-04-25 20:32:10 -0700

    cleaning up code a bit

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import PermanentDrawerLeft from "./components/Menu";
 
 //ADD PAGES.
 import Home from "./pages/Home";
 import leaderboard from "./pages/LeaderBoard";
 import ChallengeSet1 from "./pages/ChallengeSet1";
 import Selection from "./pages/Selection";
-
 import MenuBar from "./components/Menu";
 import ChallengeSetPage from "./pages/ChallengeSetPage";
 import EnvironmentPage from "./pages/EnvironmentPage";
 import { useAuth0 } from "@auth0/auth0-react";
 import UserNotAuthorized from "./pages/error-pages/userNotAuthorized";
 import React from "react";
-import { AppBar } from "@material-ui/core";
 import ChallengeBar from "./components/AppBar";
 
 export default function App() {
