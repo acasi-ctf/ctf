3fc6aee6d453ee7a0743ef1130901e247792ccaa
Colby <colby.cocking@radixiot.com>
2021-09-21 14:16:47 -0700

    working on getting challenge cards on homepage.

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,38 +1,46 @@
 import React from "react";
 import "./Home.css";
 import UserNotAuthorized from "./error-pages/userNotAuthorized";
-import { useAuth0 } from "@auth0/auth0-react";
+import {useAuth0} from "@auth0/auth0-react";
 import TrendingChallengeCards from "../components/TrendingChallengeCards";
 import Grid from "@material-ui/core/Grid";
 import Box from '@material-ui/core/Box';
 
 export default function Home() {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const {isAuthenticated, getAccessTokenSilently} = useAuth0();
 
-  if (!isAuthenticated) return <UserNotAuthorized />;
-  return (
-    <div>
-      <div className="Home">
+    if (!isAuthenticated) return <UserNotAuthorized/>;
+    return (
+        <div>
 
-          {/*TODO Landing Page Welcome */}
-          <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={2}>
-                  <h1>Home</h1>
-                  <Grid item xs={12}>
-                      <p>
-                          Welcome to Cyber Literacy For All. This is where we are going to put a really heartfelt introduction that
-                          will help users understand how our program can aid them in learning the many facets of hacking.
-                      </p>
-                  </Grid>
-                  <Grid item xs={12}>
 
-                      <TrendingChallengeCards/>
-                  </Grid>
-              </Grid>
-          </Box>
+            {/*TODO Landing Page Welcome */}
 
-          {/*TODO Dynamic list of popular challenges.*/}
-      </div>
-    </div>
-  );
+            <div id="home" class={"introText"}>
+                <p>
+                    Welcome to Cyber Literacy For All. This is where we are going to put a really heartfelt
+                    introduction that
+                    will help users understand how our program can aid them in learning the many facets of hacking.
+                </p>
+            </div>
+            <Box sx={{flexGrow: 1}}>
+                <Grid container spacing={2}>
+                    <div>
+                        <Grid item xs={12} class="trendingTitle">
+                            <h1>Trending Challenges</h1>
+                        </Grid>
+                        <Grid item xs={12}>
+
+                            <TrendingChallengeCards/>
+                        </Grid>
+                    </div>
+                </Grid>
+
+            </Box>
+
+
+            {/*TODO Dynamic list of popular challenges.*/}
+
+        </div>
+    );
 }
