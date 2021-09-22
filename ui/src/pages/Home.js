627bf625726db375a692aef7f3cc4fe5cc25a1e3
Colby <colby.cocking@radixiot.com>
2021-09-21 19:51:17 -0700

    cc added "trending" challenge cards. Landing page css modified.

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -2,45 +2,45 @@ import React from "react";
 import "../style/Home.css";
 import UserNotAuthorized from "./error-pages/userNotAuthorized";
 import {useAuth0} from "@auth0/auth0-react";
-import TrendingChallengeCards from "../components/TrendingChallengeCards";
 import Grid from "@material-ui/core/Grid";
 import Box from '@material-ui/core/Box';
+import Card1 from "../components/temp/card1";
+import Card2 from "../components/temp/card2";
+import Card3 from "../components/temp/card3";
 
 export default function Home() {
     const {isAuthenticated, getAccessTokenSilently} = useAuth0();
 
     if (!isAuthenticated) return <UserNotAuthorized/>;
     return (
-        <div>
-
-
+        <>
             {/*TODO Landing Page Welcome */}
 
-            <div id="home" class={"introText"}>
-                <p>
+            <Box sx={{flexGrow: 1}} >
+                <p class={"introText"}>
                     Welcome to Cyber Literacy For All. This is where we are going to put a really heartfelt
                     introduction that
                     will help users understand how our program can aid them in learning the many facets of hacking.
                 </p>
-            </div>
-            <Box sx={{flexGrow: 1}}>
-                <Grid container spacing={2}>
-                    <div>
-                        <Grid item xs={12} class="trendingTitle">
-                            <h1>Trending Challenges</h1>
-                        </Grid>
-                        <Grid item xs={12}>
-
-                            <TrendingChallengeCards/>
-                        </Grid>
-                    </div>
-                </Grid>
-
             </Box>
-
-
-            {/*TODO Dynamic list of popular challenges.*/}
-
-        </div>
+            <Box sx={{flexGrow: 1}} id={"trendingChallengeCards"}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} class="trendingTitle">
+                        <h1>Trending Challenges</h1>
+                    </Grid>
+                </Grid>
+                <Grid  container justifyContent="space-evenly" alignItems={"center"}>
+                    <Grid item xs={4}>
+                        <Card1/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card2/>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card3/>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
     );
 }
