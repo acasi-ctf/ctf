3de8db22c5d84e8e3a3a2e9be574cf2dbab0431b
Richard <lehoangdung29111998@gmail.com>
2021-09-23 17:48:35 -0700

    Dynamic UI on Side Menu

--- ui/src/components/SubMenu.js
+++ ui/src/components/SubMenu.js
@@ -1,40 +1,52 @@
-import React,{useState} from 'react'
-import {Link} from 'react-router-dom'
-import * as core from '@material-ui/core'
-import './SubMenu.css'
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
+import * as core from '@material-ui/core';
+import './SubMenu.css';
+import useFetchAuth from "../useFetchAuth";
+import * as UI from '@material-ui/icons';
 
+
+const iconClosed = <UI.ArrowDropDown/>;
+const iconOpened = <UI.ArrowDropUp/>;
+const ChallengeSet_Icon = <UI.CollectionsBookmark/>;
+const ChallengeIcon = <UI.Assignment/>;
 
 export default function SubMenu(props) {
-    const [subnav, setSubNav] = useState(false)
-    const item = props.item
+    const [subnav, setSubNav] = useState(false);
+    const item = props.item;
     const showSubmenu = () =>{
-        if(item.subMenu){
-            setSubNav(!subnav)
-        }
-        props.changeTitle(item.title)
+        setSubNav(!subnav);
+        // props.changeTitle(item.title);
     }
+    //constructing the API path then fetch data from there
+    const APIpath = props.path+"/"+item.slug+"/challenges";
+    const { data, error, loading } = useFetchAuth(APIpath);
+    console.log(data);
     return (
         <>
             <core.ListItem style={{display:'flex', flexDirection:'column', alignItems:'flex-start'}}>
                 
-                <Link to={item.path} style={{textDecoration:'none'}} onClick={showSubmenu} >
+                <Link to={item.path?item.path:"."} style={{textDecoration:'none'}} onClick={()=>{setSubNav(!subnav)}} >
                     <div style={{display:'flex', flexDirection: 'row'}}>
-                        <core.ListItemIcon style={{minWidth:'0'}}>{item.icon}</core.ListItemIcon>
-                        <core.ListItemText style={{color:'#000000', marginLeft:'16px', marginRight:'25px'}} primary={<core.Typography style={{fontSize:'15px'}}>{item.title}</core.Typography>} />   
+                        <core.ListItemIcon style={{minWidth:'0'}}>{item.icon?item.icon:ChallengeSet_Icon}</core.ListItemIcon>
+                        <core.ListItemText style={{color:'#000000', marginLeft:'16px', marginRight:'25px'}}
+                        primary={<core.Typography style={{fontSize:'15px'}}>{item.name}</core.Typography>} />   
                         <div>
-                            {item.subMenu && subnav ? 
-                            item.iconOpened: item.subMenu ? 
-                            item.iconClosed:null}
+                            {(item.subMenu || data.length !==0) && subnav ? iconOpened:
+                            ( item.subMenu || data.length !==0) ? iconClosed:
+                            null}
                         </div>
                     </div>
                     
                 </Link> 
-                {subnav && item.subMenu.map((item, index)=>{
+                {subnav && data.map((item)=>{
                     return (
-                        <Link to={item.path} key={index} className='subItem' onClick={()=> props.changeTitle(item.title)}>
+                        // <Link to={item.path} key={item.id} className='subItem'>
+                        <Link to={"."} key={item.id} className='subItem' >
                             <div style={{display:'flex', flexDirection:'row'}}>
-                                <core.ListItemIcon style={{minWidth:'0'}}>{item.icon}</core.ListItemIcon>
-                                <core.ListItemText style={{marginLeft:'16px', marginRight:'25px'}} primary={<core.Typography style={{fontSize:'13px'}}>{item.title}</core.Typography>} />
+                                <core.ListItemIcon style={{minWidth:'0'}}>{ChallengeIcon}</core.ListItemIcon>
+                                <core.ListItemText style={{marginLeft:'16px', marginRight:'25px'}} 
+                                primary={<core.Typography style={{fontSize:'13px'}}>{item.name}</core.Typography>} />
                             </div>
                         </Link>
                     )    
