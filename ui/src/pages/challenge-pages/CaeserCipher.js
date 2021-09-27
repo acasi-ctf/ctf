2a0a50a113a87acc6ef308879d8a4561d3d72495
Dung Le <59951881+DungLe2911@users.noreply.github.com>
2021-09-26 23:03:54 -0700

    RL: This is a testing commit

--- ui/src/pages/challenge-pages/CaeserCipher.js
+++ ui/src/pages/challenge-pages/CaeserCipher.js
@@ -67,7 +67,7 @@ export default function CaeserCipher() {
 	const {data, error, loading} = temp;
 	console.log(data);
 
-	//Testing git commit
+	//Testing git commit for automatically add initials
 
 	//LEARN THIS FOR FUTURE WORK:
 	//USESTATE HOOK WILL TRIGGER WHEN THE ASSIGNED FUNCTION IS CALL (EXAMPLE BELOW IS SETMARKDOWN/ SETPATH)
