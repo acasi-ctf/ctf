f3de9c209e1eda68b8383b2437159c5b90a9ae20
Logan Gorence <logan@gorence.io>
2021-10-02 14:44:58 -0700

    lg: uploadEnvironmentTemplate will now create EnvTemplate resources.

--- operator/src/main/kotlin/org/acasictf/ctf/operator/service/ProvisioningService.kt
+++ operator/src/main/kotlin/org/acasictf/ctf/operator/service/ProvisioningService.kt
@@ -2,24 +2,26 @@ package org.acasictf.ctf.operator.service
 
 import com.google.protobuf.Empty
 import com.google.protobuf.Timestamp
-import io.fabric8.kubernetes.api.model.ListOptions
 import io.fabric8.kubernetes.client.KubernetesClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.acasictf.ctf.operator.*
 import org.acasictf.ctf.operator.abstractions.PersistenceLayer
+import org.acasictf.ctf.operator.model.Kubernetes
+import org.acasictf.ctf.operator.model.kubernetes.v1alpha1.EnvTemplate
+import org.acasictf.ctf.operator.model.kubernetes.v1alpha1.EnvTemplateList
 import org.acasictf.ctf.operator.persistence.EnvironmentDao
 import org.acasictf.ctf.operator.persistence.ZipChallengeTemplate
 import org.acasictf.ctf.operator.provisioner.kubernetes.KubernetesProvisioner
 import org.acasictf.ctf.proto.Common
 import org.acasictf.ctf.proto.Ctfoperator.*
-import org.acasictf.ctf.proto.CtfoperatorInternal.Environment
 import org.acasictf.ctf.proto.CtfoperatorInternal.ProvisionerType
 import org.acasictf.ctf.proto.EnvironmentProvisioningServiceGrpcKt
 import java.io.File
 import java.io.File.createTempFile
 import java.time.Instant
 import java.util.*
+import org.acasictf.ctf.proto.CtfoperatorInternal.Environment as EnvironmentProto
 
 class ProvisioningService(
     private val kube: KubernetesClient,
@@ -27,6 +29,7 @@ class ProvisioningService(
 ) : EnvironmentProvisioningServiceGrpcKt.
 EnvironmentProvisioningServiceCoroutineImplBase() {
     private val envDao = EnvironmentDao(persistenceLayer.database())
+    private val envTemplateClient = kube.resources(EnvTemplate::class.java, EnvTemplateList::class.java)
 
     override suspend fun startEnvironment(request: StartEnvironmentRequest):
             StartEnvironmentResponse = managed {
@@ -40,7 +43,7 @@ EnvironmentProvisioningServiceCoroutineImplBase() {
             nanos = now.nano
         }.build()
 
-        val env = Environment.newBuilder().apply {
+        val env = EnvironmentProto.newBuilder().apply {
             createdTime = ts
             lastPingTime = ts
             provisionerDone = false
@@ -73,7 +76,6 @@ EnvironmentProvisioningServiceCoroutineImplBase() {
             it.id == request.challengeId.contents
         } ?: return@managed failureResponse
 
-        // TODO: Assuming Kubernetes-only.
         val prov = KubernetesProvisioner(envIdStr, kube)
         try {
             prov.provision(c)
@@ -85,7 +87,7 @@ EnvironmentProvisioningServiceCoroutineImplBase() {
         return@managed StartEnvironmentResponse.newBuilder().apply {
             successBuilder.apply {
                 environmentIdBuilder.apply {
-                    contents = envIdStr
+                    contents = "chl-$envIdStr"
                 }
             }
         }.build()
@@ -93,22 +95,7 @@ EnvironmentProvisioningServiceCoroutineImplBase() {
 
     override suspend fun stopEnvironment(request: StopEnvironmentRequest)
             : StopEnvironmentResponse = managed {
-        envDao.get(request.environmentId)
-            ?: throw Exception("Missing environment")
-        val envIdStr = request.environmentId.contents
-
-        val listOptions = ListOptions().apply {
-            labelSelector = "ctf-env-id=$envIdStr"
-        }
-        val pods = kube.pods().inNamespace(kubeNamespace).list(listOptions)
-
-        logger.info("Deleting ${pods.items.size} pods for environment $envIdStr")
-        kube.pods().inNamespace(kubeNamespace).delete(pods.items)
-
-        logger.info("Deleting persistent environment $envIdStr")
-        envDao.remove(request.environmentId)
-
-        return@managed StopEnvironmentResponse.newBuilder().build()
+        TODO("Not implemented")
     }
 
     override suspend fun uploadEnvironmentTemplate(request: UploadEnvironmentTemplateRequest)
@@ -127,6 +114,29 @@ EnvironmentProvisioningServiceCoroutineImplBase() {
         val challengeZipFile = File(getChallengesDir(), "$id.zip")
         challengeZipFile.createNewFile()
         challengeZipFile.writeBytes(request.envZip.toByteArray())
+
+        val templates = challengeTemplate.challenges.map {
+            val kubernetesJson = challengeTemplate.readChallengeJson(
+                it.slug,
+                "kubernetes.json",
+                Kubernetes.serializer()
+            )!!
+            if (kubernetesJson.templateCRDVersion != "v1alpha1") {
+                throw Exception("Unsupported CRD version")
+            }
+
+            val templateYamlIs = challengeTemplate.readChallengeFile(
+                it.slug,
+                kubernetesJson.templatePath
+            ).getInputStream()
+
+            envTemplateClient.load(templateYamlIs).get().apply {
+                metadata.name = it.id
+                metadata.namespace = kubeNamespace
+            }
+        }
+        templates.forEach { envTemplateClient.dryRun(true).create(it) }
+        templates.forEach { envTemplateClient.dryRun(false).create(it) }
 
         logger.info("New challenge template uploaded: $challengeZipFile")
 
