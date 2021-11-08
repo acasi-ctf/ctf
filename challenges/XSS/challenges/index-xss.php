d12a9bda97833ef96f3cda38683a372116fcfc62
EllisHZH <48583260+EllisHZH@users.noreply.github.com>
2021-11-07 19:29:44 -0800

    Add a index page
    A index page for user to select challenges

---
+++ challenges/XSS/challenges/index-xss.php
@@ -0,0 +1,21 @@
+<?php include("base-2.html")?>
+
+<?php
+
+    echo "<div style=\"text-align:center;\">";
+    
+    echo '<a href = "xss-1-1.php"> Refelction XSS Challenge </a>';
+
+    echo "<br>";
+    
+    echo '<a href = "xss-2.php"> DOM XSS Challenge </a>';
+
+    echo "<br>";
+    
+    echo '<a href = "xss-3-pre.php"> Stored XSS Challenge </a>';
+
+    echo "<br>";
+    
+    echo "</div>";
+
+?>
--- challenges/XSS/challenges/xss-3-pre.php
+++ challenges/XSS/challenges/xss-3-pre.php
@@ -13,11 +13,9 @@ mysqli_query($con, "DELETE FROM `chat_history` WHERE 1");
 mysqli_query($con, "INSERT INTO `chat_history`(`id`, `name`, `message`) VALUES ('1','Alex','Hello World!')");
 
 echo "<center>";
-echo "<h1>Click to start XSS challenge</h1>";
+echo "<h1>Click to start Stored XSS challenge</h1>";
 
-echo "<form action=xss-3.php method=GET>
-    <input type=submit name=submit value=Go />
-    </form>";
+echo "<h1><a href = \"xss-3.php\"> Start </a></h1>";
 
 echo"</center>";
 
