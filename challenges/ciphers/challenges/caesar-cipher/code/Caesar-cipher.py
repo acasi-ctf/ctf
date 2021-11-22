ad96678ce964a972cb06e85f7121bd1921a89c57
EllisHZH <48583260+EllisHZH@users.noreply.github.com>
2021-11-21 18:56:12 -0800

    Add encode Flag
    Insert flag for each cipher challenge. Each flag will show as base64 format before the challenge finished.

--- challenges/ciphers/challenges/caesar-cipher/code/Caesar-cipher.py
+++ challenges/ciphers/challenges/caesar-cipher/code/Caesar-cipher.py
@@ -1,4 +1,5 @@
 import random
+import base64
 
 #Random String
 def random_alphabet():
@@ -30,6 +31,14 @@ def Caesar(flag,number):
 
     return after
 
+def convert():
+
+    s ="Q0FFUy1BUkNILUFMTEUtTkdFMQ=="
+    b = s.encode("ascii")
+    b64 = base64.b64decode(b)
+    output = b64.decode("ascii")
+    print(output)
+
 def main():
 
     number = random.randint(1,10)
@@ -51,7 +60,8 @@ def main():
             print("Nope!Try Again!")
             continue
 
-    print("Congratulations! You get 10 points!")
+    print("Congratulations! The flag is:")
+    convert()
 
 if __name__ == "__main__":
     main()
