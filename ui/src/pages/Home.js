9ac5586750fafaf5b207734435db3d1a2e14afb4
aditevon <aditya.kumar@evontech.com>
2021-12-07 19:56:20 +0530

    Commit Home Page API Integration

--- ui/src/pages/Home.js
+++ ui/src/pages/Home.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/Home.css";
 import PreLoginPage from "./error-pages/preLoginPage";
 import { useAuth0 } from "@auth0/auth0-react";
 import {Button, Card, Nav} from "react-bootstrap";
+import {Link} from 'react-router-dom'
 
 export default function Home() {
   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  //const [dataLength, setDataLength] = useState(0);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [trendingChallengeData, setTrendingChallengeData] = useState([]);
+  const [topChallengeData, setTopChallengeData] = useState([]);
+  const [gameChallengeData, setGameChallengeData] = useState([]);
+  useEffect(()=>{
+    fetch('/api/trending-challenges')
+    .then(res => res.json())
+    .then(
+      (data) => {
+        setIsLoaded(true);
+        setTrendingChallengeData(data);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  },[]);
+
+  useEffect(()=>{
+    fetch('/api/top-challenges')
+    .then(res => res.json())
+    .then(
+      (data) => {
+        setIsLoaded(true);
+        setTopChallengeData(data);
+        console.log('Top Challenge Data =>');
+        console.log(data);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  },[]);
+
+  useEffect(()=>{
+    fetch('/api/challenge-sets/games/challenges')
+    .then(res => res.json())
+    .then(
+      (data) => {
+        console.log(data);
+        setIsLoaded(true);
+        setGameChallengeData(data);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  },[]);
 
   if (!isAuthenticated) return <PreLoginPage />;
   return (
@@ -21,59 +75,20 @@ export default function Home() {
           <div className="challengesCol cHomeCol">
             <div className="container">
               <div className="row flexWrap">
+              {trendingChallengeData.map((trending, trendingIndex) => (
                 <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
+                  <Link to={"/play/"+trending.challengeSet.slug+"/"+trending.challenge.slug}>
                     <Card>
-                      <Card.Img variant="null" src="challenges/c001.jpg" alt="thumb" className="mw-100"/>
+                      <Card.Img variant="null" src={"challenges/c00"+ (trendingIndex === 0 ? 2 : trendingIndex) +".jpg"} alt="thumb" className="mw-100"/>
                       <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Caesar Cipher
+                        <Card.Text className="text-center" key={trendingIndex}>
+                        {trending.challenge.name}
                         </Card.Text>
                       </Card.Body>
                     </Card>
-                  </Nav.Link>
+                  </Link>
                 </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c002.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Letter to Number Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c003.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Morse Code Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c004.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Reverse Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-
-
-        
-
+                  ))}
               </div>
             </div>
           </div>
@@ -82,55 +97,20 @@ export default function Home() {
           <div className="challengesCol cHomeCol">
             <div className="container">
               <div className="row flexWrap">
+              {topChallengeData.map((popular, topIndex) => (
                 <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
+                  <Link to={"/play/"+popular.challengeSet.slug+"/"+popular.challenge.slug}>
                     <Card>
-                      <Card.Img variant="null" src="challenges/c001.jpg" alt="thumb" className="mw-100"/>
+                    <Card.Img variant="null" src={"challenges/c00"+ (topIndex === 0 ? 2 : topIndex) +".jpg"} alt="thumb" className="mw-100"/>
                       <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Caesar Cipher
+                        <Card.Text className="text-center" key={topIndex}>
+                        {popular.challenge.name}
                         </Card.Text>
                       </Card.Body>
                     </Card>
-                  </Nav.Link>
+                  </Link>
                 </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c002.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Letter to Number Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c003.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Morse Code Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c004.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Reverse Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>       
+                  ))}
 
               </div>
             </div>
@@ -140,55 +120,20 @@ export default function Home() {
           <div className="challengesCol cHomeCol">
             <div className="container">
               <div className="row flexWrap">
+              {gameChallengeData.map((javagame, gameIndex) => (
                 <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
+                   <Link to={"/play/games/"+javagame.slug}>
                     <Card>
-                      <Card.Img variant="null" src="challenges/c001.jpg" alt="thumb" className="mw-100"/>
+                      <Card.Img variant="null" src={"challenges/c00"+ (gameIndex === 0 ? 2 : gameIndex) +".jpg"} alt="thumb" className="mw-100"/>
                       <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Caesar Cipher
+                        <Card.Text className="text-center" key={gameIndex}>
+                        {javagame.name}
                         </Card.Text>
                       </Card.Body>
                     </Card>
-                  </Nav.Link>
+                  </Link>
                 </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c002.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Letter to Number Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c003.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Morse Code Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>
-
-                <div className="col-xl-3 col-lg-6 col-md-12">
-                  <Nav.Link href="#">
-                    <Card>
-                      <Card.Img variant="null" src="challenges/c004.jpg" alt="thumb" className="mw-100"/>
-                      <Card.Body className="p-0">
-                        <Card.Text className="text-center">
-                          Reverse Cipher
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Nav.Link>
-                </div>       
+                  ))}
 
               </div>
             </div>
